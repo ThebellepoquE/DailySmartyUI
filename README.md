93bdc9622a1f842be228857db8747bcfa19495d6
ThebellepoquE <thebellepoque@gmail.com>
2025-09-08 17:52:00 +0200

    set up routes

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
-# React/Redux/Router Starter Project
+# DailySmartyUI
 
-> Provided for the students of the [Bottega Code School](https://bottega.tech/)
+Este proyecto es parte del bootcamp de DevCamp, una réplica de la interfaz de usuario de DailySmarty.
 
-*Fork from [es6-webpack2-starter](https://github.com/micooz/es6-webpack2-starter)*
+## Agradecimientos
+
+Un especial agradecimiento a:
+
+- **Jordan Hudgens** y **DevCamp Bootcamp** ([Bottega Code School](https://bottega.tech/)) por proporcionar una educación excepcional y guía en el desarrollo web
+- **maxnerlson997** por sus valiosas contribuciones y recursos
+- La **Comunidad Open Source** por hacer posible este proyecto a través de sus herramientas y librerías
+
+## Tecnologías Utilizadas
+
+- React 16
+- Redux
+- React Router
+- Bootstrap 4
+- Webpack
+- SCSS
+- Redux Form
+
+## Proyecto Base
+
+Este proyecto está construido sobre js-builder, una base de código esqueleto para aplicaciones React/Redux/Router.
+*Basado en [es6-webpack2-starter](https://github.com/micooz/es6-webpack2-starter)*
+
+## Licencia
+
+MIT
+
+---
+*Este proyecto es parte de un proceso educativo y está destinado a fines de aprendizaje.*
--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm} from 'redux-form';
 
 class SearchBar extends Component {
 
-  handleFormSubmit = function(query) {
-    console.log('trying to handle submit for query', query);
-  }
+    handleFormSubmit = function({query}) {
+        console.log('trying to handle submit for query', query);
+        //navigate to a new route.
+    }
 
-  renderInput(field) {
-    return <input type="text" placeholder="Search DailySmarty"/>
-  }
+    renderInput(field) {
+        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+    }
 
-  render() {
+    render() {
 
-    const { handleSubmit } = this.props;
+        const { handleSubmit } = this.props;
 
-    return (
-      <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-        <Field name="query" component={this.renderInput}/>
-      </form>
-    )
-  }
+        return (
+            <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <Field name="query" component={this.renderInput}/>
+            </form>
+        )
+    }
 }
 
 SearchBar = reduxForm({
-  form: 'searchBar'
+    form: 'searchBar'
 })(SearchBar);
 
 export default SearchBar;
